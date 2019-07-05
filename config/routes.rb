652b0fb44e3a8c5c7c72466cb7893211c0e94f50
pamccombs <pa.mccombs1@gmail.com>
2019-07-05 16:34:23 -0400

    Update application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#home'
  
-  resources :games, only: [:index, :show, :new, :create] do
+  resources :games do
     resources :reviews
   end
   resources :users
