3fe20ceb60cb53dff1680398435940f859fd2e4e
pamccombs <pa.mccombs1@gmail.com>
2019-07-05 16:36:13 -0400

    Create reviews controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,48 @@
+class ReviewsController < ApplicationController
+  
+  def index
+    @game = which_game?
+    @reviews = @game.reviews
+  end
+
+  def new
+    @game = which_game?
+    @review = Review.new
+  end
+
+  def create
+    @game = which_game?
+    @review = Review.create(review_params)
+  end
+
+  def show
+    @game = which_game?
+    @review = Review.find(params[:id])
+  end
+
+  def edit
+    @game = which_game?
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @game = which_game?
+    @review = Review.find(params[:id])
+    @review.update(review_params)
+    redirect_to game_review_path(@game, @review)
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @game = which_game?
+    @review.destroy
+    redirect_to game_path(@game)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :body, :rating, :game_id, :user_id)
+  end
+
+end
