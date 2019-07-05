b830b4a33ccdef074a8caa82ad64741183b63e41
pamccombs <pa.mccombs1@gmail.com>
2019-07-05 16:35:48 -0400

    Create games controller and views

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,28 @@
+class GamesController < ApplicationController
+  
+  def index
+    @games = Game.all
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.create(game_params)
+    redirect_to game_path(@game)
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    @review = Review.new
+  end
+
+
+  private
+
+  def game_params
+    params.require(:game).permit(:title, :genre, :platform)
+  end
+
+end
