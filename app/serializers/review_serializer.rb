5f2e70babb70315771f651acdd68b48dacd290ec
pamccombs <pa.mccombs1@gmail.com>
2019-08-24 00:34:07 -0400

    add attribute options to serializer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,14 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :user_id, :game_id
+  attributes :id, :title, :body, :user_id, :game_id, :game
   belongs_to :user
   belongs_to :game
+
+  def game
+    {game_title: self.object.game.title}
+  end 
+
+  #def review
+    #{review_link: self.object.review.}
+  #end
+
 end
